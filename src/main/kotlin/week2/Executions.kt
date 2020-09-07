7ffc7471472c9a1f192a14e6734488a58eb17db0
Woo <jhsw0375@gmail.com>
2020-09-07 20:50:12 +0900

    [Solve] LeetCode Easy
    - First Unique String
    - ImplementstrStr
    - PlusOne

---
+++ src/main/kotlin/week2/Executions.kt
@@ -0,0 +1,12 @@
+package week2
+
+/**
+ * created by victory_woo on 2020/09/06
+ * */
+fun main(args: Array<String>){
+    //ValidAnagram().main(args)
+    //ValidPalindrome().main(args)
+    //FirstUniqueString().main(args)
+    //ImplementStr().main(args)
+    PlusOne().main(args)
+}
