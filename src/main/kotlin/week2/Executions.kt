7472d7f77a60ea189dde9be6d109967f99563262
Woo <jhsw0375@gmail.com>
2020-09-08 12:17:32 +0900

    [Solve] LeetCode Easy - IntersectionOfArray - LongestCommonPrefix - MoveZeroes - SingleNumber

--- src/main/kotlin/week2/Executions.kt
+++ src/main/kotlin/week2/Executions.kt
@@ -3,10 +3,15 @@ package week2
 /**
  * created by victory_woo on 2020/09/06
  * */
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
     //ValidAnagram().main(args)
     //ValidPalindrome().main(args)
     //FirstUniqueString().main(args)
     //ImplementStr().main(args)
-    PlusOne().main(args)
+    //PlusOne().main(args)
+    //SingleNumber().main(args)
+    //IntersectionOfArray().main(args)
+    //MoveZeroes().main(args)
+    //TwoSum().main(args)
+    LongestCommonPrefix().main(args)
 }
