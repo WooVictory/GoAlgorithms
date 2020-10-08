b0a5eff56f2257de67436ff7f3f6e2b8dd19df65
Woo <jhsw0375@gmail.com>
2020-10-08 17:53:10 +0900

    feat. week5 leetCode solve

---
+++ src/main/java/week5/NumberOf1Bits/woo.java
@@ -0,0 +1,18 @@
+package week5.NumberOf1Bits;
+
+/**
+ * created by victory_woo on 2020/10/08
+ */
+public class woo {
+    public static void main(String[] args) {
+        System.out.println(hammingWeight(00000000000000000000000000001011));
+        System.out.println(hammingWeight(00000000000000000000000010000000));
+    }
+
+    public static int hammingWeight(int n) {
+        System.out.println(Integer.toBinaryString(n));
+        int count=0;
+
+        return count;
+    }
+}
